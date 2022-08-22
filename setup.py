656d202ca9642c9c6da5addbe2667198e22486c7
Chris Vernon <chrisrvernon@gmail.com>
2022-08-22 18:00:49 -0400

    initial package setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+import re
+from setuptools import setup, find_packages
+
+
+def readme():
+    """Return the contents of the project README file."""
+
+    with open('README.md') as f:
+        return f.read()
+
+
+version = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", open('basd/__init__.py').read(), re.M).group(1)
+
+setup(
+    name='basd',
+    version=version,
+    packages=find_packages(),
+    url='https://github.com/JGCRI/basd',
+    license='BSD-2-Clause',
+    author='Noah Prime',
+    author_email='noah.prime@pnnl.gov',
+    description='',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    python_requires='>=3.8.*, <4',
+    include_package_data=True,
+    install_requires=[
+        "numpy>=1.23",
+        "PyYAML>=6",
+    ],
+    extras_require={
+        'dev': [
+        ]
+    }
+)
