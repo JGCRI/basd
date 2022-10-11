312e13820e325cf085e50bacd05c28e066866a93
PNL\prim232 <noah.prime@pnnl.gov>
2022-10-11 16:31:24 -0400

    For plotting empirical CDFS

--- basd/one_loc_output.py
+++ basd/one_loc_output.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+import plotly.express as px
 import seaborn as sns
 import xarray as xr
 
@@ -6,18 +8,25 @@ import basd.ba_params as par
 
 
 class BaLocOutput:
-    def __init__(self, sim_fut_ba: xr.DataArray, sim_fut: xr.DataArray,
+    def __init__(self,
+                 sim_fut_ba: xr.DataArray, sim_fut: xr.DataArray,
+                 obs_hist: xr.DataArray, sim_hist: xr.DataArray,
                  variable: str, params: par.Parameters):
+
         # Turn DataArrays into DataFrames
+        oh_df = obs_hist.to_dataframe().reset_index()
+        sh_df = sim_hist.to_dataframe().reset_index()
         ba_df = sim_fut_ba.to_dataframe().reset_index()
-        df = sim_fut.to_dataframe().reset_index()
+        sf_df = sim_fut.to_dataframe().reset_index()
 
         # Add column to signify the source of the time series
+        oh_df['Source'] = 'Observed Historical'
+        sh_df['Source'] = 'Simulated Historical'
         ba_df['Source'] = 'Simulated Future Bias Adjusted'
-        df['Source'] = 'Simulated Future'
+        sf_df['Source'] = 'Simulated Future'
 
         # Concatenate into full DataFrame
-        self.time_series = pd.concat([ba_df, df], ignore_index=True)
+        self.time_series = pd.concat([ba_df, sf_df, oh_df, sh_df], ignore_index=True)
 
         # Set basic attributes
         self.params = params
@@ -25,8 +34,10 @@ class BaLocOutput:
 
     def plot_hist(self, scale=None, bins=15, palette='light:m_r', style='white', xlab=None,
                   title=None):
+        indexes = (self.time_series['Source'] == 'Simulated Future') | (
+                    self.time_series['Source'] == 'Simulated Future Bias Adjusted')
         sns.set_style(style=style)
-        p = sns.histplot(data=self.time_series, x=self.variable, hue='Source', bins=bins,
+        p = sns.histplot(data=self.time_series.loc[indexes], x=self.variable, hue='Source', bins=bins,
                          multiple='dodge', palette=palette)
         if scale:
             p.set_yscale(scale)
@@ -34,3 +45,20 @@ class BaLocOutput:
             p.set_xlabel(xlab)
         if title:
             p.set_title(title)
+
+    def plot_ecdf(self, **kwargs):
+
+        # Getting data within thresholds
+        within_thresh = np.ones(len(self.time_series.index))
+        if self.params.lower_threshold:
+            above = self.params.lower_threshold < self.time_series[self.variable]
+            within_thresh = np.logical_and(above, within_thresh)
+        if self.params.upper_threshold:
+            below = self.params.upper_threshold > self.time_series[self.variable]
+            within_thresh = np.logical_and(below, within_thresh)
+
+        # Plot Empirical CDF with plotly
+        p = px.ecdf(self.time_series.loc[within_thresh],
+                    x=self.variable, color='Source', **kwargs)
+
+        return p
