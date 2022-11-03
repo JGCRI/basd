66746e8a4dee81ce6d2f24c2fda0db184dd4c3d5
PNL\prim232 <noah.prime@pnnl.gov>
2022-11-03 15:15:22 -0400

    Unit test script for the statistical downscaling utility functions

---
+++ basd/tests/test_sd_utils.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+
+import pandas as pd
+import xarray as xr
+
+from basd import Downscaler
+from basd import Parameters
+import basd.sd_utils as sdu
+
+
+class StatisticalDownscalingTest(unittest.TestCase):
+    DATA_PATH = '../data'
+    PR_OBS_HIST = xr.open_dataset(os.path.join(DATA_PATH,
+                                               'pr_obs-hist_fine_1979-2014.nc'))
+    PR_SIM_FUT = xr.open_dataset(os.path.join(DATA_PATH,
+                                              'pr_sim-fut_coarse_2065-2100.nc'))
+    PR_PARAMS = Parameters(lower_bound=0, lower_threshold=0.0000011574,
+                           trend_preservation='mixed', distribution='gamma',
+                           if_all_invalid_use=0)
+
+
+if __name__ == '__main__':
+    unittest.main()
