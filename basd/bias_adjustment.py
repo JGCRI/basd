67fbd7d9a9b3ce2507ec5c127760e17c6f6bbb31
Noah Prime <noahprime21@gmail.com>
2023-10-09 06:49:05 -0700

    Compute after transpose fixes a bug

--- basd/bias_adjustment.py
+++ basd/bias_adjustment.py
@@ -500,7 +500,7 @@ def save_adjustment_nc(sim_fut_ba, input_calendar, variable, output_dir, day_fil
         sim_fut_ba[variable].attrs = variable_attrs
 
     # Make sure we've computed
-    # sim_fut_ba = sim_fut_ba.transpose('time', 'lat', 'lon')
+    sim_fut_ba = sim_fut_ba.transpose('time', 'lat', 'lon')
     sim_fut_ba = sim_fut_ba.compute()    
 
     # If not saving daily data in long term
@@ -516,7 +516,7 @@ def save_adjustment_nc(sim_fut_ba, input_calendar, variable, output_dir, day_fil
         AttributeError('Unable to convert calendar')
 
     # Save daily data
-    sim_fut_ba = sim_fut_ba.transpose('time', 'lat', 'lon', ...)
+    # sim_fut_ba = sim_fut_ba.transpose('time', 'lat', 'lon', ...)
     sim_fut_ba[['time', 'lat', 'lon', variable]].to_netcdf(os.path.join(output_dir, day_file), encoding=encoding)
     sim_fut_ba.close()
 
