e78797b5f6bd8f28ac30be6cbda838b5fc72fde4
crvernon <chrisrvernon@gmail.com>
2023-11-15 11:10:19 -0500

    clean content for docs hosting

--- basd/__init__.py
+++
@@ -1,8 +0,0 @@
-from .bias_adjustment import *
-from .statistical_downscaling import *
-from .utils import *
-from .ba_params import *
-from .one_loc_output import *
-from .regridding import *
-
-__version__ = "0.0.0"
